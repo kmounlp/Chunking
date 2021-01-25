bd4f84ba8c82bf58f2d1896f869087e01f89088b
ho-lol <35889609+ho-lol@users.noreply.github.com>
2021-01-25 14:08:00 +0900

    Delete KoreanDependencyParserusingStackPointer directory

--- KoreanDependencyParserusingStackPointer/neuronlp2/io/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'max'
-
-from .alphabet import *
-from .instance import *
-from .logger import *
-from .writer import *
-from . import conllx_data
-from . import conllx_stacked_data
-from . import conll03_data
